6c9370805fa1977edc653228bb1e9edb1ecb7354
RobsonAndradev <robsom.s.a@gmail.com>
2021-07-07 18:24:33 -0300

    Fix dependency installation order

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,12 @@
 REGION=$1
 CLUSTER_NAME=$2
 CMD=$3
-KUBE_VERSION=$(curl -L -s https://dl.k8s.io/release/stable.txt)
 
+apk add --no-cache --update openssl curl ca-certificates
 pip install awscli
 aws eks update-kubeconfig --name $CLUSTER_NAME --region $REGION
 
-chmod +x /entrypoint.sh
-apk add --no-cache --update openssl curl ca-certificates
+KUBE_VERSION=$(curl -L -s https://dl.k8s.io/release/stable.txt)
 curl -o /usr/local/bin/kubectl  \
   -L https://storage.googleapis.com/kubernetes-release/release/$KUBE_VERSION/bin/linux/amd64/kubectl
 chmod +x /usr/local/bin/kubectl
